ad10cb10075add1db4d2c0224e9e32766ad1cc41
JeanFeitani <jeanls.folks@gmail.com>
2024-04-02 14:40:37 -0300

    primeira aula finalizada

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,11 @@
 import { Header } from './components/header'
+import { AttendeeList } from './components/attendee-list'
 
 export function App() {
   return (
-    <div className="max-w-[1216px] mx-auto py-5">
-      <Header></Header>
+    <div className="max-w-[1216px] mx-auto py-5 flex flex-col">
+      <Header />
+      <AttendeeList />
     </div>
   )
 }
